bfc019fced693c1fb88e21630a8f68a4037e4139
Maria <naplesaries2014@gmail.com>
2023-02-09 12:53:51 +0100

    fix home layout

--- react/src/components/Banner.jsx
+++ react/src/components/Banner.jsx
@@ -1,25 +1,26 @@
-import { Box, Grid, Typography, List, ListItemText } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import SearchBar from "./SearchBar";
+
 import { useNavigate } from "react-router-dom";
 
 export default function Banner({ setCompany }) {
   const navigate = useNavigate();
   return (
-    <Box
+    <Grid
+      container
+      spacing={2}
       sx={{
-        alignItems: "stretch",
+        justifyContent: "center",
+        height: { xs: "auto", md: "100vh" },
+        textAlign: { xs: "center", md: "left", lg: "left" },
         backgroundImage: `url('${process.env.PUBLIC_URL}/assets/background.png')`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         backgroundSize: "cover",
-        height: "auto",
-        maxWidth: "100%",
-        fontSize: "0.875rem",
-        fontWeight: "700",
       }}
     >
-      <Grid item xs={6} md={4}>
-        <Typography sx={{ ml: 4 }}>
+      <Grid item xs={12} sx={{ textAlign: "start", ml: 1 }}>
+        <Typography>
           <img
             src="../assets/ja-logo.png"
             alt="jobadvisorlogo"
@@ -29,66 +30,62 @@ export default function Banner({ setCompany }) {
           />
         </Typography>
       </Grid>
-      <Grid container sx={{ justifyContent: "center" }}>
-        <Grid item md={5} lg={6} sx={{ my: 5, mx: 3 }}>
-          <Typography
-            sx={{
-              textAlign: "center",
-              fontFamily: "sans-serif",
-              typography: {
-                md: "h3",
-                sm: "h4",
-                xs: "h5",
-              },
-            }}
-          >
-            Discover a company DNA
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            sx={{
-              textAlign: "center",
-              fontFamily: "sans-serif",
-              typography: {
-                md: "h3",
-                sm: "h4",
-                xs: "h5",
-              },
-            }}
-          >
-            to find the perfect match for You!
-          </Typography>
-          <List>
-            <ListItemText>
-              <Typography
-                lineHeight={2}
-                sx={{
-                  mt: 3,
-                  textAlign: "center",
-                  typography: { md: "body1", sm: "body2" },
-                }}
-              >
-                Job Advisor helps companies promote the right values and job
-                seekers make the right career choice.
-              </Typography>
-            </ListItemText>
-            <SearchBar setCompany={setCompany}></SearchBar>
-          </List>
-        </Grid>
-        <Grid item md={5} lg={4} sx={{ mb: 5 }}>
-          <Typography>
-            <img
-              src="../../assets/figma download/edited-idea.png"
-              alt="women"
-              style={{
-                width: "100%",
-                height: "auto",
-                textAlign: "center",
-              }}
-            />
-          </Typography>
-        </Grid>
+      <Grid item xs={12} md={10} lg={10}>
+        <Typography
+          sx={{
+            // textAlign: { xs: "center", md: "left", lg: "center" },
+            fontFamily: "sans-serif",
+            typography: {
+              lg: "h2",
+              md: "h3",
+              sm: "h4",
+              xs: "h5",
+            },
+          }}
+        >
+          Discover a company DNA
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          sx={{
+            // textAlign: "left",
+            fontFamily: "sans-serif",
+            typography: {
+              lg: "h2",
+              md: "h3",
+              sm: "h4",
+              xs: "h5",
+            },
+          }}
+        >
+          to find the perfect match for You!
+        </Typography>
       </Grid>
-    </Box>
+      <Grid item xs={12} md={8} lg={10}>
+        <Typography
+          sx={{
+            typography: { md: "body1", sm: "body2" },
+          }}
+        >
+          Job Advisor helps companies promote the right values and job seekers
+          make the right career choice.
+        </Typography>
+      </Grid>
+      <Grid item xs={10} md={6} lg={6} sx={{ mt: 1 }}>
+        <SearchBar setCompany={setCompany}></SearchBar>
+      </Grid>
+
+      <Grid item xs={12} md={4} lg={4}>
+        <img
+          src="../../assets/figma download/edited-idea.png"
+          alt="women"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        />
+      </Grid>
+    </Grid>
   );
 }
--- react/src/components/SearchBar.jsx
+++ react/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import { getCompanyListVm } from "../api/viewModelels/companyListVm";
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import * as React from "react";
 export default function SearchBar({ setCompany }) {
@@ -11,41 +11,33 @@ export default function SearchBar({ setCompany }) {
   const [value, setValue] = useState(undefined);
   const [inputValue, setInputValue] = useState();
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        mt: 7,
-      }}
-    >
-      <Grid container sx={{ justifyContent: "center" }}>
-        <Grid item xs={6} md={6} lg={8} sx={{ mr: 1 }}>
-          <Autocomplete
-            value={value}
-            onChange={(event, newValue) => {
-              data.includes(newValue) && setValue(newValue);
-              setCompany(newValue);
-              navigate("/companyOverview");
-            }}
-            disablePortal
-            inputValue={inputValue}
-            onInputChange={(event, newInputValue) => {
-              setInputValue(newInputValue);
-            }}
-            id="controllable-states-demo"
-            options={data}
-            sx={{ alignSelf: "center" }}
-            renderInput={(params) => (
-              <TextField
-                sx={{ backgroundColor: "white", maxWidth: "100%" }}
-                {...params}
-                label="company name"
-                onClick={() => getCompanyListVm().then((v) => setData(v))}
-              />
-            )}
-          />
-        </Grid>
+    <Grid container>
+      <Grid item xs={12} md={6} lg={8}>
+        <Autocomplete
+          value={value}
+          onChange={(event, newValue) => {
+            data.includes(newValue) && setValue(newValue);
+            setCompany(newValue);
+            navigate("/companyOverview");
+          }}
+          disablePortal
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          id="controllable-states-demo"
+          options={data}
+          sx={{ alignSelf: "center" }}
+          renderInput={(params) => (
+            <TextField
+              sx={{ backgroundColor: "white", maxWidth: "100%" }}
+              {...params}
+              label="company name"
+              onClick={() => getCompanyListVm().then((v) => setData(v))}
+            />
+          )}
+        />
       </Grid>
-    </Box>
+    </Grid>
   );
 }
